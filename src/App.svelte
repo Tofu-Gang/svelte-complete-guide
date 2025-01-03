a3a675b8da732b002fab7223da8fa19b419ba55a
Jakub Franěk <tofugangsw@gmail.com>
2025-01-03 22:32:01 +0100

    7. Svelte Components Basic Structure introduce global styles use js variable in tag attribute parse html from js variable

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script>
-  let name = "Svelte";
+  const name = "Svelte";
+  const lorem_picsum_src = "https://picsum.photos/200";
+  const string = "Some <strong>string</strong> with html.";
 </script>
 
+<img src={lorem_picsum_src} alt="Some text {name.toLowerCase()}" />
 <h1>Hello {name.toUpperCase()}!</h1>
+<p>{@html string}</p>
 
 <style>
   h1 {
