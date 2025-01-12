4aa2f28e869594984969897785395ac3bfd7e570
Jakub Franěk <tofugangsw@gmail.com>
2025-01-12 13:23:48 +0100

    13. Using SCSS in a Svelte Project Using svelte-preprocess

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,13 +1,16 @@
 <button><slot>Default Text</slot></button>
 
-<style>
-    button {
-        border: none;
-        background-color: #ff3e00;
-        color: #ffffff;
-        padding: 15px 20px;
-        font-weight: bold;
-        border-radius: 5px;
-        cursor: pointer;
+<style lang="scss">
+  button {
+    border: none;
+    background-color: #ff3e00;
+    color: #ffffff;
+    padding: 15px 20px;
+    font-weight: bold;
+    border-radius: 5px;
+    cursor: pointer;
+    &:hover {
+      background-color: variables.$color;
+      }
     }
 </style>
